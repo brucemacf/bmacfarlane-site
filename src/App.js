64ce2b9388ae7a75e4bf874298b831a30a4203ab
Bruce MacFarlane <brucemacfarlane@Bruces-MacBook-Pro.local>
2018-10-28 18:50:15 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{display: 'flex'}}>
+        <div className="content">
+          <div className="title">Bruce MacFarlane</div>
+          <div>* site coming soon</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
